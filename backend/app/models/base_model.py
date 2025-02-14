169056d8c25a92b1cc116071a584a8d1271fbd9c
ChrisMT <christophermoralestorres06@gmail.com>
2025-02-14 10:32:49 -0500

    adding basemodel

---
+++ backend/app/models/base_model.py
@@ -0,0 +1,39 @@
+from motor.motor_asyncio import AsyncIOMotorCollection
+from pydantic import BaseModel, Field
+from uuid import UUID, uuid4
+import pendulum
+
+class BaseEntity(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
+    created_at: str = Field(default_factory=lambda: pendulum.now("UTC").to_iso8601_string())
+    updated_at: str = Field(default_factory=lambda: pendulum.now("UTC").to_iso8601_string())
+
+
+    def update_timestamp(self):
+        "Updates the updated_at field with the current date in UTC"
+        
+        self.updated_at = pendulum.now("UTC").to_iso8601_string()
+
+    def to_timezone(self, timezone: str):
+        "Convert timestamps to a specific time zone"
+
+        return {
+            "id": self.id,
+            "created_at": pendulum.parse(self.created_at).in_timezone(timezone).to_iso8601_string(),
+            "updated_at": pendulum.parse(self.updated_at).in_timezone(timezone).to_iso8601_string(),
+        }
+    
+    async def save(self, collection: AsyncIOMotorCollection):
+        """Save or update the document in MongoDB"""
+
+        self.updated_at = pendulum.now("UTC").to_iso8601_string()
+        data = self.model_dump()
+        await collection.update_one({"id": self.id}, {"$set": data}, upsert=True)
+
+
+    async def update(self, collection: AsyncIOMotorCollection, data: dict):
+        """Update specific fields and save"""
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        await self.save(collection)
